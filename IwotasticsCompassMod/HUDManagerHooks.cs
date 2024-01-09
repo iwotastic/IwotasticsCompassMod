632598acb4d426c7e84931b37c65b101c15e6009
Ian Morrill <iwotastic.on.github@ianmorrill.com>
2024-01-08 21:50:22 -0500

    Ensured compat with v47, adjusted clock position so no overlap happens, version bump

--- IwotasticsCompassMod/HUDManagerHooks.cs
+++ IwotasticsCompassMod/HUDManagerHooks.cs
@@ -14,8 +14,8 @@ public class HUDManagerHooks
         // Create HUD game object once and parent it under the HUDContainer
         var hudCompassObject = new GameObject();
         hudCompassObject.transform.SetParent(__instance.HUDContainer.transform, false);
-        var newPos = new Vector3(0f, 185f, 0f);
-        hudCompassObject.transform.localPosition = newPos;
+        var newCompassPos = new Vector3(0f, 193f, 0f);
+        hudCompassObject.transform.localPosition = newCompassPos;
 
         // Add our own script to the compass object, so it can do compass things
         var hudCompassBehaviour = hudCompassObject.AddComponent<HUDCompassBehaviour>();
@@ -23,5 +23,12 @@ public class HUDManagerHooks
         // This is blursed, but I can't think of a better way to get the font that the game uses
         hudCompassBehaviour.compassFontAsset =
             __instance.weightCounter == null ? null : __instance.weightCounter.font;
+        
+        // Push the clock down a little so that the compass doesn't clip over it anymore
+        var clockTransform = __instance.Clock.canvasGroup.transform;
+        var oldClockPos = clockTransform.localPosition;
+        var newClockPos = new Vector3(oldClockPos.x, oldClockPos.y - 27, oldClockPos.z);
+        Debug.Log($"Set clock position to {newClockPos}");
+        clockTransform.localPosition = newClockPos;
     }
 }
