fb1a2ea3fdc7e9caea63c33a710c878fa810db30
Entity378 <150670165+Entity378@users.noreply.github.com>
2024-01-16 21:13:05 +0100

    Update Plugin.cs

--- IwotasticsCompassMod/Plugin.cs
+++ IwotasticsCompassMod/Plugin.cs
@@ -15,7 +15,7 @@ public class Plugin : BaseUnityPlugin
     {
         CompassPosX = Config.Bind<float>("General", "CompassPosX", 0f, "X coordinate of the compass position (0 means it's in the center of the screen)");
         CompassPosY = Config.Bind<float>("General", "CompassPosY", 185f, "Y coordinate of the compass position (0 means it's in the center of the screen)");
-        ShouldMoveClockDown = Config.Bind<bool>("General", "ShouldMoveClockDown", true, "Push the clock down a little so that the compass doesn't clip over it.\n (automatically disabled if the position of the clock is not the default one)");
+        ShouldMoveClockDown = Config.Bind<bool>("General", "ShouldMoveClockDown", true, "Push the clock down a little so that the compass doesn't clip over it");
 
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} (v{MyPluginInfo.PLUGIN_VERSION}) was loaded successfully.");
 
