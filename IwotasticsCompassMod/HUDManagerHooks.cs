1f0b7e794f6e709485c478ae194a8f938568d9e7
Entity378 <150670165+Entity378@users.noreply.github.com>
2024-01-16 20:53:49 +0100

    Update HUDManagerHooks.cs

--- IwotasticsCompassMod/HUDManagerHooks.cs
+++ IwotasticsCompassMod/HUDManagerHooks.cs
@@ -12,14 +12,26 @@ public class HUDManagerHooks
         // Create HUD game object once and parent it under the HUDContainer
         var hudCompassObject = new GameObject();
         hudCompassObject.transform.SetParent(__instance.HUDContainer.transform, false);
-        var newCompassPos = new Vector3(Plugin.CompassXpos.Value, Plugin.CompassYpos.Value, 0f);
+        var newCompassPos = new Vector3(Plugin.CompassPosX.Value, Plugin.CompassPosY.Value, 0f);
         hudCompassObject.transform.localPosition = newCompassPos;
 
         // Add our own script to the compass object, so it can do compass things
         var hudCompassBehaviour = hudCompassObject.AddComponent<HUDCompassBehaviour>();
-       
+
         // This is blursed, but I can't think of a better way to get the font that the game uses
         hudCompassBehaviour.compassFontAsset =
             __instance.weightCounter == null ? null : __instance.weightCounter.font;
+
+        // Push the clock down a little so that the compass doesn't clip over it anymore if the config is true and the position is not changed
+        if (Plugin.ShouldMoveClockDown.Value == true && Plugin.CompassPosY.Value == 185f && Plugin.CompassPosX.Value == 0f)
+        {
+            var fixedCompassPos = new Vector3(Plugin.CompassPosX.Value, (Plugin.CompassPosY.Value + 8f), 0f);
+            hudCompassObject.transform.localPosition = fixedCompassPos;
+            var clockTransform = __instance.Clock.canvasGroup.transform;
+            var oldClockPos = clockTransform.localPosition;
+            var newClockPos = new Vector3(oldClockPos.x, oldClockPos.y - 27, oldClockPos.z);
+            Debug.Log($"Set clock position to {newClockPos}");
+            clockTransform.localPosition = newClockPos;
+        }
     }
 }
